26880537839755e0bee0be3900edf4e3eb1e0b4b
Arinjoy_Programmer <arinjoy1234@gmail.com>
2022-03-23 22:12:31 +0530

    added basic calculation commands and normal commands will be created soon

---
+++ commands/commands.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+
+
+class Commands(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+
+
+
+
+def setup(client):
+    client.add_cog(Commands(client))
